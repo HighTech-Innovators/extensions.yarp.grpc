868a2591c3f1f6d40ba416a743383c95f79dfdcf
Florin <florin.deleanu@hightechict.nl>
2025-03-05 14:36:03 +0100

    remake program.cs, move to Central Package Management

--- src/Extensions.Yarp.Grpc.Service/Program.cs
+++ src/Extensions.Yarp.Grpc.Service/Program.cs
@@ -22,11 +22,23 @@ builder.Services.AddScoped<CombinerService>();
 builder.Services.AddSingleton<YarpConfig>();
 
 // Add YARP services
-var yarpConfig = new YarpConfig(builder.Configuration);
-var inMemory=new InMemoryConfigProvider(yarpConfig.GetRoutes().Result, yarpConfig.GetClusters());
-builder.Services.AddSingleton<IProxyConfigProvider>(inMemory);
+//var yarpConfig = new YarpConfig(builder.Configuration);
+//var inMemory=new InMemoryConfigProvider(yarpConfig.GetRoutes().Result, yarpConfig.GetClusters());
+//builder.Services.AddSingleton<IProxyConfigProvider>(inMemory);
 builder.Services.AddReverseProxy();
-builder.Services.AddSingleton(inMemory);
+//builder.Services.AddSingleton(inMemory);
+
+builder.Services.AddSingleton<IProxyConfigProvider>(serviceProvider =>
+{
+    var yarpConfig = serviceProvider.GetRequiredService<YarpConfig>();
+    var inMemory = new InMemoryConfigProvider(yarpConfig.GetRoutes().Result, yarpConfig.GetClusters());
+    return inMemory;
+});
+builder.Services.AddSingleton(serviceProvider =>
+{
+    var yarpConfig = serviceProvider.GetRequiredService<YarpConfig>();
+    return new InMemoryConfigProvider(yarpConfig.GetRoutes().Result, yarpConfig.GetClusters());
+});
 builder.Services.AddHostedService<ServiceMonitor>();
 
 var app = builder.Build();
