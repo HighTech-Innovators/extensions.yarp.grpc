b3f0f8b1841dcb1bfc3a7c6cb26172f70e94f895
Florin <florin.deleanu@hightechict.nl>
2025-03-12 15:01:27 +0100

    update e2e test, fix live route updating

--- end-to-end-tests/e2e_tests.sh
+++ end-to-end-tests/e2e_tests.sh
@@ -9,16 +9,92 @@ fi
 
 server1=$1
 
+# Function to check the gRPC reflection endpoint
+check_grpc_reflection() {
+  response=$(grpcurl -plaintext $server1 list)
+  echo "Response from gRPC reflection endpoint: $response"
 
-# Call the gRPC reflection endpoint
-response=$(grpcurl -plaintext $server1 list)
-echo "Response from gRPC reflection endpoint: $response"
+  if [[ $response == *"TestServerGreeterOne"* && $response == *"TestServerGreeterTwo"* ]]; then
+    echo "gRPC reflection endpoint verification successful."
+    return 0
+  elif [[ $response == *"TestServerGreeterOne"* ]]; then
+    echo "Only TestServerGreeterOne found"
+    return 1
+  else
+    echo "No greeter found in the response"
+    return 1
+  fi
+}
 
-# Verify the response
-if [[ $response == *"TestServerGreeterOne"* && $response == *"TestServerGreeterTwo"* ]]; then
-  echo "gRPC reflection endpoint verification successful."
-  exit 0
-else
-  echo "gRPC reflection endpoint verification failed."
+# checks the routing works for both greeters
+verify_routing_works(){
+  echo "Checking routing works for both greeters..."
+  response1=$(grpcurl -plaintext $server1 testGreet.TestServerGreeterOne.SayHelloOne)
+  response2=$(grpcurl -plaintext $server1 testGreetTwo.TestServerGreeterTwo.SayHelloTwo)
+
+  if [[ $response1 == *"Hello one"* && $response2 == *"Hello two"* ]]; then
+    echo "Routing verification successful."
+    return 0
+  else
+    echo "Routing verification failed."
+    return 1
+  fi
+}
+
+# Function to retry another function
+retry() {
+  local -r -i max_attempts="$1"
+  local -r function_name="$2"
+  local -i attempt=1
+
+  while [[ $attempt -le $max_attempts ]]; do
+    echo "Attempt $attempt of $max_attempts for $function_name..."
+    $function_name
+    result=$?
+    if [[ $result -eq 0 ]]; then
+      return 0
+    # elif [[ $result -eq 1 ]]; then
+      # return 1
+    fi
+    echo "Retrying $function_name in 10 seconds..."
+    sleep 10
+    attempt=$((attempt + 1))
+  done
+
+  echo "All attempts to run $function_name failed."
+  return 1
+}
+
+# Retry logic for gRPC reflection
+retry 4 check_grpc_reflection
+result=$?
+if [[ $result -eq 1 ]]; then
   exit 1
 fi
+
+# Retry logic for routing verification
+retry 4 verify_routing_works
+routing_result=$?
+exit $routing_result
+
+# # Retry logic
+# max_attempts=4
+# attempt=1
+# while [[ $attempt -le $max_attempts ]]; do
+#   echo "Attempt $attempt of $max_attempts..."
+#   check_grpc_reflection
+#   result=$?
+#   if [[ $result -eq 0 ]]; then
+#     verify_routing_works
+#     routing_result=$?
+#     exit $routing_result
+#   elif [[ $result -eq 1 ]]; then
+#     exit 1
+#   fi
+#   echo "Retrying in 10 seconds..."
+#   sleep 10
+#   attempt=$((attempt + 1))
+# done
+
+# echo "All attempts failed."
+# exit 1
--- src/Extensions.Yarp.Grpc/DI.cs
+++ src/Extensions.Yarp.Grpc/DI.cs
@@ -4,7 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using Yarp.ReverseProxy.Configuration;
 
 namespace Extensions.Yarp.Grpc
@@ -18,19 +20,30 @@ namespace Extensions.Yarp.Grpc
             builderServices.AddSingleton<YarpConfig>();
 
             // Add YARP services
+            var logger = builderServices.BuildServiceProvider().GetRequiredService<ILogger<YarpConfig>>();
+            var builderConfig = builderServices.BuildServiceProvider().GetRequiredService<IConfiguration>();
+            var yarpConfig = new YarpConfig(builderConfig, logger);
+            var inMemory = new InMemoryConfigProvider(yarpConfig.GetRoutes().Result, yarpConfig.GetClusters());
+            builderServices.AddSingleton<IProxyConfigProvider>(inMemory);
             builderServices.AddReverseProxy();
+            builderServices.AddSingleton(inMemory);
 
-            builderServices.AddSingleton<IProxyConfigProvider>(serviceProvider =>
-            {
-                var yarpConfig = serviceProvider.GetRequiredService<YarpConfig>();
-                var inMemory = new InMemoryConfigProvider(yarpConfig.GetRoutes().Result, yarpConfig.GetClusters());
-                return inMemory;
-            });
-            builderServices.AddSingleton(serviceProvider =>
-            {
-                var yarpConfig = serviceProvider.GetRequiredService<YarpConfig>();
-                return new InMemoryConfigProvider(yarpConfig.GetRoutes().Result, yarpConfig.GetClusters());
-            });
+            ////todo why doesnt this update the yarp config?
+            //// Add YARP services
+            //builderServices.AddReverseProxy();
+
+            //builderServices.AddSingleton<IProxyConfigProvider>(serviceProvider =>
+            //{
+            //    var yarpConfig = serviceProvider.GetRequiredService<YarpConfig>();
+            //    var inMemory = new InMemoryConfigProvider(yarpConfig.GetRoutes().Result, yarpConfig.GetClusters());
+            //    return inMemory;
+            //});
+            //builderServices.AddSingleton(serviceProvider =>
+            //{
+            //    var yarpConfig = serviceProvider.GetRequiredService<YarpConfig>();
+            //    return new InMemoryConfigProvider(yarpConfig.GetRoutes().Result, yarpConfig.GetClusters());
+            //});
+
             builderServices.AddHostedService<ServiceMonitor>();
         }
 
